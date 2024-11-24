184b7c62fb7af4ef95e785656697d0e37797c753
CarsonYuen <cyuen64@uwo.ca>
2024-11-23 23:02:15 -0500

    search function updated

--- SQL/ex5.sql
+++ SQL/ex5.sql
@@ -1,63 +1,75 @@
-DELIMITER $$
+DELIMITER //
 
-CREATE PROCEDURE SearchPatientData(
-    IN searchDietaryRestrictions VARCHAR(255),
-    IN searchAllergy VARCHAR(255),
-    IN searchCondition VARCHAR(255),
-    IN searchDrugName VARCHAR(255),
-    IN searchPatientName VARCHAR(255),
-    IN minDOB DATE, 
-    IN maxDOB DATE,
-    IN insuranceProvider VARCHAR(255)
+CREATE PROCEDURE Search_All(
+    IN search_term VARCHAR(255)
 )
 BEGIN
-    SELECT 
-        p.PatientID,
-        CONCAT(p.firstName, ' ', p.lastName) AS PatientName,
-        p.DateOfBirth,
-        p.Sex,
-        p.Weight,
-        pa.streetNo, pa.streetName, pa.unitNo, pa.city, pa.state, pa.country AS Address,
-        pi.provider AS InsuranceProvider,
-        pc.medicalCondition AS MedicalCondition,
-        a.allergyName AS Allergy,
-        pm.medID AS MedicationID,
-        m.medName AS MedicationName,
-        mp.schedule AS MealPlanSchedule,
-        f.foodname AS DietaryRestriction
-    FROM 
-        Patient p
-    LEFT JOIN 
-        PatientAddress pa ON p.AddressID = pa.AddressID
-    LEFT JOIN 
-        Insurance pi ON p.PatientID = pi.PatientID
-    LEFT JOIN 
-        PatientCondition pc ON p.PatientID = pc.patientID
-    LEFT JOIN 
-        PatientAllergy paa ON p.PatientID = paa.patientID
-    LEFT JOIN 
-        Allergy a ON paa.allergyName = a.allergyName
-    LEFT JOIN 
-        PatientMedication pm ON p.PatientID = pm.patientID
-    LEFT JOIN 
-        Medication m ON pm.medID = m.medID
-    LEFT JOIN 
-        MealPlan mp ON p.MealPlanID = mp.MealPlanID
-    LEFT JOIN 
-        Meal ml ON mp.MealPlanID = ml.MealPlanID
-    LEFT JOIN 
-        food f ON ml.foodName1 = f.foodname
-    WHERE 
-        (searchDietaryRestrictions IS NULL OR f.foodname LIKE CONCAT('%', searchDietaryRestrictions, '%'))
-        AND (searchAllergy IS NULL OR a.allergyName LIKE CONCAT('%', searchAllergy, '%'))
-        AND (searchCondition IS NULL OR pc.medicalCondition LIKE CONCAT('%', searchCondition, '%'))
-        AND (searchDrugName IS NULL OR m.medName LIKE CONCAT('%', searchDrugName, '%'))
-        AND (searchPatientName IS NULL OR CONCAT(p.firstName, ' ', p.lastName) LIKE CONCAT('%', searchPatientName, '%'))
-        AND (minDOB IS NULL OR p.DateOfBirth >= minDOB)
-        AND (maxDOB IS NULL OR p.DateOfBirth <= maxDOB)
-        AND (insuranceProvider IS NULL OR pi.provider LIKE CONCAT('%', insuranceProvider, '%'))
-    ORDER BY 
-        p.PatientID;
-END $$
+    -- Search the Patient table
+    SELECT * FROM Patient 
+    WHERE PatientID LIKE CONCAT('%', search_term, '%') 
+    OR firstName LIKE CONCAT('%', search_term, '%') 
+    OR lastName LIKE CONCAT('%', search_term, '%')
+    OR Sex LIKE CONCAT('%', search_term, '%') 
+    OR DateOfBirth LIKE CONCAT('%', search_term, '%')
+    OR Height LIKE CONCAT('%', search_term, '%')
+    OR Weight LIKE CONCAT('%', search_term, '%');
+    
+    -- Search the PatientAddress table
+    SELECT * FROM PatientAddress
+    WHERE AddressID LIKE CONCAT('%', search_term, '%') 
+    OR StreetNo LIKE CONCAT('%', search_term, '%') 
+    OR StreetName LIKE CONCAT('%', search_term, '%')
+    OR UnitNo LIKE CONCAT('%', search_term, '%') 
+    OR City LIKE CONCAT('%', search_term, '%')
+    OR State LIKE CONCAT('%', search_term, '%')
+    OR Country LIKE CONCAT('%', search_term, '%');
+    
+    -- Search the Staff table
+    SELECT * FROM Staff
+    WHERE staffID LIKE CONCAT('%', search_term, '%')
+    OR firstName LIKE CONCAT('%', search_term, '%')
+    OR lastName LIKE CONCAT('%', search_term, '%') 
+    OR position LIKE CONCAT('%', search_term, '%') 
+    OR department LIKE CONCAT('%', search_term, '%');
+    
+    -- Search the Medication table
+    SELECT * FROM Medication
+    WHERE medID LIKE CONCAT('%', search_term, '%') 
+    OR medName LIKE CONCAT('%', search_term, '%') 
+    OR drugclass LIKE CONCAT('%', search_term, '%')
+    OR adminDetails LIKE CONCAT('%', search_term, '%') 
+    OR storagedetails LIKE CONCAT('%', search_term, '%');
+    
+    -- Search the PatientCondition table
+    SELECT * FROM PatientCondition
+    WHERE patientID LIKE CONCAT('%', search_term, '%')
+    OR medicalCondition LIKE CONCAT('%', search_term, '%') 
+    OR description LIKE CONCAT('%', search_term, '%')
+    OR diagnoserID LIKE CONCAT('%', search_term, '%');
+    
+    -- Search the Allergy table
+    SELECT * FROM Allergy
+    WHERE allergyName LIKE CONCAT('%', search_term, '%')
+    OR managementStrategy LIKE CONCAT('%', search_term, '%')
+    OR seasonalconsiderations LIKE CONCAT('%', search_term, '%');
+    
+    -- Search the Visit table
+    SELECT * FROM Visit
+    WHERE visitID LIKE CONCAT('%', search_term, '%')
+    OR visitorID LIKE CONCAT('%', search_term, '%')
+    OR patientID LIKE CONCAT('%', search_term, '%') 
+    OR VisitDate LIKE CONCAT('%', search_term, '%') 
+    OR notes LIKE CONCAT('%', search_term, '%');
+    
+    -- Add additional SELECT queries for any other tables as needed
+    -- For example, if you want to search the food table:
+    SELECT * FROM food
+    WHERE foodname LIKE CONCAT('%', search_term, '%')
+    OR foodgroup LIKE CONCAT('%', search_term, '%')
+    OR calories LIKE CONCAT('%', search_term, '%')
+    OR protein LIKE CONCAT('%', search_term, '%')
+    OR fats LIKE CONCAT('%', search_term, '%');
+    
+END //
 
 DELIMITER ;
