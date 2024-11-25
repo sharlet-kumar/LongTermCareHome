28c889654a173c3d10c053b6c586cc24d46efc10
CarsonYuen <cyuen64@uwo.ca>
2024-11-24 23:49:36 -0500

    ex7 fixed

--- SQL/ex7.sql
+++ SQL/ex7.sql
@@ -1,3 +1,4 @@
+use longtermcare;
 DROP VIEW IF EXISTS PatientView;
 DROP VIEW IF EXISTS MedicationView;
 -- View 1: PatientView
@@ -16,7 +17,7 @@ FROM
     Patient;
 
 -- Query on PatientView
-SELECT * FROM PatientView WHERE Sex = 'M' AND DateOfBirth = '1980-01-01';
+SELECT * FROM PatientView WHERE Sex = 'M' AND DateOfBirth = '1974-03-31';
 
 
 -- View 2: MedicationView
@@ -31,19 +32,10 @@ FROM
     Medication;
 
 -- Query on MedicationView
-SELECT * FROM MedicationView WHERE medName = 'aspirin';
+SELECT * FROM MedicationView WHERE medName = 'Wonder';
 
 -- Testing views with insert
-
--- Insert into PatientView
-INSERT INTO Patient (PatientID, firstName, lastName, DateOfBirth, Sex, Height, Weight, AddressID, DNR, InsuranceCheck)
-VALUES 
-    ('P001', 'John', 'Doe', '1980-01-01', 'M', 175, 80, NULL, TRUE, TRUE),
-    ('P002', 'Jane', 'Smith', '1990-05-15', 'F', 160, 60, NULL, FALSE, FALSE),
-    ('P003', 'Mark', 'Lee', '1985-03-22', 'M', 180, 85, NULL, TRUE, TRUE);
-
 -- Insert into MedicationView
 INSERT INTO Medication (MedID, MedName, DrugClass, AdminDetails, StorageDetails)
 VALUES 
-    ('M001', 'Aspirin', 'Analgesic', 'Oral', 'Store at room temperature'),
-    ('M002', 'Penicillin', 'Antibiotic', 'Injectable', 'Refrigerated');
+    ('M999', 'Wonder', 'Analgesic', 'Oral', 'Store at room temperature');
